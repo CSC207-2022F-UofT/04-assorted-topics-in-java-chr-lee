5a1eac6335c8325cbe26f00fa618c9da1c74c3ff
chr-lee <chrislee6260@gmail.com>
2022-10-09 23:58:11 -0400

    GitHub Exercise 4. Assorted Topics in Java

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -10,4 +10,45 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import java.util.ArrayList;
 import java.util.List;
+
+
+public class DrivableTrader<T> extends Trader<T> {
+
+    private final List<T> inventory;
+    private final List<T> wishlist;
+    /**
+     * Construct a DrivableTrader, giving them the given inventory,
+     * wishlist, and money.
+     *
+     * @param inventory Objects in this DrivableTrader's inventory
+     * @param wishlist  Objects in this DrivableTrader's wishlist
+     * @param money     The DrivableTrader's money
+     */
+    public DrivableTrader(List<T> inventory, List<T> wishlist, int money) {
+        super(money);
+        this.inventory = inventory;
+        this.wishlist = wishlist;
+    }
+
+    public void addToWishlist(T item) {
+        if (item instanceof Drivable) {
+            this.wishlist.add(item);
+        }
+    }
+    public void AddToInventory(T item) {
+        if (item instanceof Drivable) {
+            this.inventory.add(item);
+        }
+    }
+
+    public int getSellingPrice(T item){
+        if (item instanceof Tradable) {
+            if (item instanceof Drivable) {
+                return ((Tradable) item).getPrice() + ((Drivable) item).getMaxSpeed();
+            }
+        }
+        return Tradable.MISSING_PRICE;
+    }
+}
